9ce64a788c6d5db1e247680f0577206579602cb2
JayM1926 <71045580+JayM1926@users.noreply.github.com>
2023-03-31 19:46:58 +0100

    update - updated functionality of tickets and notes

--- Server/config/db.js
+++ Server/config/db.js
@@ -1,16 +1,16 @@
-const mongoose = require("mongoose");
-const config = require("config");
-const db = config.get("mongoURI");
+// const mongoose = require("mongoose");
+// const config = require("config");
+// const db = config.get("mongoURI");
 
-const connectDB = async () => {
-	try {
-		await mongoose.connect(db);
+// const connectDB = async () => {
+// 	try {
+// 		await mongoose.connect(db);
 
-		console.log("MongoDB connected");
-	} catch (error) {
-		console.error(error.message);
-		process.exit(1);
-	}
-};
+// 		console.log("MongoDB connected");
+// 	} catch (error) {
+// 		console.error(error.message);
+// 		process.exit(1);
+// 	}
+// };
 
-module.exports = connectDB;
+// module.exports = connectDB;
--- Server/server.js
+++ Server/server.js
@@ -1,28 +1,28 @@
-const express = require("express");
-const cors = require("cors");
-const colors = require("colors");
-const PORT = process.env.PORT || 8001;
-const asyncMySQL = require("./mysql/connection");
-const sqlQueries = require("./mysql/queriesUser");
+// const express = require("express");
+// const cors = require("cors");
+// const colors = require("colors");
+// const PORT = process.env.PORT || 8001;
+// const asyncMySQL = require("./mysql/connection");
+// const sqlQueries = require("./mysql/queriesUser");
 
-const app = express();
+// const app = express();
 
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+// app.use(cors());
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => {
-	res.send("Hello there");
-});
+// app.get("/", (req, res) => {
+// 	res.send("Hello there");
+// });
 
-app.use((req, res, next) => {
-	req.asyncMySQL = asyncMySQL;
-	next();
-});
+// app.use((req, res, next) => {
+// 	req.asyncMySQL = asyncMySQL;
+// 	next();
+// });
 
-app.use("/api/users", require("./routes/user"));
-app.use("/api/tickets", require("./routes/tickets"));
+// app.use("/api/users", require("./routes/user"));
+// app.use("/api/tickets", require("./routes/tickets"));
 
-app.listen(PORT, () => {
-	console.log(`server started on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+// 	console.log(`server started on port ${PORT}`);
+// });
